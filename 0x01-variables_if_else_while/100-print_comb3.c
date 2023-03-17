1d140214e1504e88c9686bb85e8a034b8b0363a8
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-17 14:08:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Robinson
+ * Desription: different commination of to digit 
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * ou can only use the putchar function (every other function (printf, puts, etc…) is forbidden
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (c < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

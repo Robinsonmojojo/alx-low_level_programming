1abd6893e88afd615e7c4674c54330e1fb8ab346
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-17 10:59:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Robinson
+ * Return: 0
+ */
+int main(void)
+{	
+	char lc;
+
+	for (ls = 'a'; lc <= 'z'; lc++)
+{
+	putchar(lc);
+}
+putchar('\n');
+return (0);
+}

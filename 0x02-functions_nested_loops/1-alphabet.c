97f9fb51d43e0a716b30645da633fa4e5755038a
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-21 13:15:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ */
+
+void print_alphabet(void);
+{
+	char robin;
+
+	for (robin = 'a'; robin <= 'z'; robin++)
+		_putchar(robin);
+}

e1aa46d662730c77b468fbd42682400402d8b237
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-21 14:30:06 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - money
+ * _isalpha -  checks for alphabetic character
+ * @c:  checks for alphabetic character
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}

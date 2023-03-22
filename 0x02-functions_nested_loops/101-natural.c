af7cc98c33b1e722101079b02b41d6101b08c72c
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-21 18:40:43 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - money
+ * Description - a program that computes and prints the sum of all the multiples
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+
+		c++;
+	}
+	_putchar("%i\n", sum);
+	return (0);
+}

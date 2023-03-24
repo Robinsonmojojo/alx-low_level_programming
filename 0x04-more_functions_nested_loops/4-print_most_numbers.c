03c6085d67a5595ca10a4ddd5ab85e2a7d413246
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-24 16:18:32 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	if (c = '0', c <= '9', c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}

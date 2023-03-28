07115ce4e10fa780c76258abd70ca1b0a131ab3c
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-28 04:19:47 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - order as they are stored in the array
+ * @a: order as they are stored in the array
+ * @n: order as they are stored in the array
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(",");
+	}
+	printf('\n');
+}

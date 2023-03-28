7310873dd08c71346acf242ffe0a757cabba4354
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-28 03:53:23 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - followed by a new line
+ * @str: followed by a new lin
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int l, n, i;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+	{
+		for (i = l / 2; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	else if (l % 2)
+	{
+		for (n (l - 1) / 2; n < l - 1; i++)
+		{
+			putchar(str[n + 1]);
+		}
+	}
+	putchar('\n');
+}

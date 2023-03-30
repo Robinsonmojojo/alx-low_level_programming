13a1ba25462797372f29fc6fbfc85293b9b71d44
Robinsonmojojo <rkinyunye@gmail. com>
2023-03-30 05:40:39 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper -  lowercase lette
+ * @n:  lowercase letters
+ * Returb: 0
+ */
+
+char *string_toupper(char *)
+{
+	int m;
+
+	m = 0;
+	while (n[m] != '\0')
+	{
+		if (n[m] >= 'a' && n[m] <= 'z')
+			n[m] = n[m] - 32;
+		m++;
+	}
+	return (n)
+}

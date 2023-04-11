bf5b362693857fb0cc87035e5753014285f0b2e2
Robinsonmojojo <rkinyunye@gmail. com>
2023-04-11 01:32:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: print name
+ * @argv: sme
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
